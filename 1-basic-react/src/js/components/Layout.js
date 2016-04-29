c9aab6ec11126ed95d14e833e8c7b28058fdd472
Howard Smith <howardsmithnz@gmail.com>
2016-04-29 14:23:15 +1200

    minor edit to jsx

--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -1,26 +1,26 @@
-import React from "react";
+import React from 'react'
 
-import Footer from "./Footer";
-import Header from "./Header";
+import Footer from './Footer'
+import Header from './Header'
 
 export default class Layout extends React.Component {
-  constructor() {
-    super();
+  constructor () {
+    super()
     this.state = {
-      title: "Welcome",
-    };
+      title: "Welcome - It's working!",
+    }
   }
 
-  changeTitle(title) {
-    this.setState({title});
+  changeTitle (title) {
+    this.setState({title})
   }
 
-  render() {
+  render () {
     return (
-      <div>
-        <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
-        <Footer />
-      </div>
-    );
+    <div>
+      <Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+      <Footer />
+    </div>
+    )
   }
 }
